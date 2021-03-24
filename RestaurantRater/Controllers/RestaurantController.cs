6a39e7b83917eb998790d0f894b5574d2520681e
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-03-24 13:57:14 -0400

    Delete

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using static RestaurantRater.Models.Restaurant;
@@ -37,6 +38,35 @@ namespace RestaurantRater.Controllers
                 return RedirectToAction("Index");
             }
             return View(restaurant);
+        }
+        //get:Restaurant/Delete/(ID)
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //Post:Resrtaurant/Delete
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Delete(int id)
+        {
+           
+            
+                Restaurant restaurant = _db.Restaurants.Find(id);
+                _db.Restaurants.Remove(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            
+            
         }
     }
 }
