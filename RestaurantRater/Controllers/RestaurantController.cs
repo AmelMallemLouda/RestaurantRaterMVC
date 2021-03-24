44b33cee1b27dabef1e9ef86c6d394aa1ec2f7c7
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-03-24 19:48:30 -0400

    Edit

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -96,5 +96,20 @@ namespace RestaurantRater.Controllers
             return View(restaurant);
 
         }
+        //get:Restaurant/Details/(ID)
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
     }
 }
